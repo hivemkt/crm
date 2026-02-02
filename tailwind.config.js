2031b8d1de96bfb4460c77e0924ebd30a3919059
hivemkt <email.brunoalbuquerque@gmail.com>
2026-02-02 10:05:26 -0300

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'hive-yellow': '#f6c500',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
